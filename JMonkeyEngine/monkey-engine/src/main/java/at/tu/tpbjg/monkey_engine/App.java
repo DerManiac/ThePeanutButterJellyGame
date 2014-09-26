4e9801883cc3b95492bc90cea332f625c75b37d8
vinzynth <vinzynthnihilit@gmail.com>
2014-09-26 23:08:54 +0200

    JMonkeyEngineTest2

--- JMonkeyEngine/monkey-engine/src/main/java/at/tu/tpbjg/monkey_engine/App.java
+++ JMonkeyEngine/monkey-engine/src/main/java/at/tu/tpbjg/monkey_engine/App.java
@@ -1,22 +1,27 @@
 package at.tu.tpbjg.monkey_engine;
 
 import com.jme3.app.SimpleApplication;
+import com.jme3.material.Material;
+import com.jme3.math.ColorRGBA;
+import com.jme3.scene.Geometry;
+import com.jme3.scene.shape.Box;
 
 /**
  * Hello world!
  *
  */
-public class App extends SimpleApplication
-{
-    public static void main( String[] args )
-    {
-        App app = new App();
-        app.start();
-    }
+public class App extends SimpleApplication {
+	public static void main(String[] args) {
+		App app = new App();
+		app.start();
+	}
 
 	@Override
 	public void simpleInitApp() {
-		// TODO Auto-generated method stub
-		
+		Box b = new Box(1, 1, 1);
+		Geometry geom = new Geometry("Box", b);
+		Material mat = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
+		mat.setColor("Color", ColorRGBA.Blue);
+		rootNode.attachChild(geom);
 	}
 }

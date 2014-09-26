bb9b4b1ab3ac191b860df6bb159df447f2dc50ba
vinzynth <vinzynthnihilit@gmail.com>
2014-09-26 22:47:49 +0200

    JMonkeyEngine Test
    Kurzes Testprojekt, ob die JMonkeyEngine etwas taugt

---
+++ JMonkeyEngine/monkey-engine/src/main/java/at/tu/tpbjg/monkey_engine/App.java
@@ -0,0 +1,22 @@
+package at.tu.tpbjg.monkey_engine;
+
+import com.jme3.app.SimpleApplication;
+
+/**
+ * Hello world!
+ *
+ */
+public class App extends SimpleApplication
+{
+    public static void main( String[] args )
+    {
+        App app = new App();
+        app.start();
+    }
+
+	@Override
+	public void simpleInitApp() {
+		// TODO Auto-generated method stub
+		
+	}
+}

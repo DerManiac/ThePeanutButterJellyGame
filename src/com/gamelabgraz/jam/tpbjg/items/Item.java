6c45d8090c0eba3799b7fbddaf05ac53832ea397
Christian <leopold.christian@gmx.at>
2014-09-27 18:12:02 +0300

    Item coordinates added

--- src/com/gamelabgraz/jam/tpbjg/items/Item.java
+++ src/com/gamelabgraz/jam/tpbjg/items/Item.java
@@ -3,19 +3,43 @@ package com.gamelabgraz.jam.tpbjg.items;
 import com.gamelabgraz.jam.tpbjg.ThePeanutButterJellyGame;
 
 /**
- * @author vinzynth
- * Sep 27, 2014 - 5:36:59 PM
+ * @author vinzynth Sep 27, 2014 - 5:36:59 PM
  *
  */
 public class Item {
-  
-  private final ItemType type;
 
-  public Item(final ItemType type) {
+  private final ItemType type;
+  private int x;
+  private int y;
+
+  public Item(final int x, final int y, final ItemType type) {
+    this.x = x;
+    this.y = y;
     this.type = type;
   }
-  
-  public void processEffect(ThePeanutButterJellyGame game){
+
+  public void processEffect(ThePeanutButterJellyGame game) {
     this.type.process(game);
-  } 
+  }
+
+  /**
+   * @return the type
+   */
+  public ItemType getType() {
+    return type;
+  }
+
+  /**
+   * @return the x
+   */
+  public int getX() {
+    return x;
+  }
+
+  /**
+   * @return the y
+   */
+  public int getY() {
+    return y;
+  }
 }

423f9393802fb3ae0dcccb023e6432cc5f685865
Christian <leopold.christian@gmx.at>
2014-09-27 17:39:25 +0300

    Item Actions added

--- src/com/gamelabgraz/jam/tpbjg/items/Item.java
+++ src/com/gamelabgraz/jam/tpbjg/items/Item.java
@@ -1,5 +1,21 @@
 package com.gamelabgraz.jam.tpbjg.items;
 
-public interface Item {
+import com.gamelabgraz.jam.tpbjg.ThePeanutButterJellyGame;
 
+/**
+ * @author vinzynth
+ * Sep 27, 2014 - 5:36:59 PM
+ *
+ */
+public class Item {
+  
+  private final ItemType type;
+
+  public Item(final ItemType type) {
+    this.type = type;
+  }
+  
+  public void processEffect(ThePeanutButterJellyGame game){
+    this.type.process(game);
+  } 
 }
---
+++ src/com/gamelabgraz/jam/tpbjg/items/implementation/CometAction.java
@@ -0,0 +1,18 @@
+package com.gamelabgraz.jam.tpbjg.items.implementation;
+
+import com.gamelabgraz.jam.tpbjg.ThePeanutButterJellyGame;
+import com.gamelabgraz.jam.tpbjg.items.IItemAction;
+
+public class CometAction implements IItemAction {
+
+  /**
+   * {@inheritDoc}
+   * @see com.gamelabgraz.jam.tpbjg.items.IItemAction#process(com.gamelabgraz.jam.tpbjg.ThePeanutButterJellyGame)
+   */
+  @Override
+  public void process(ThePeanutButterJellyGame game) {
+    // TODO Auto-generated method stub
+    
+  }
+
+}

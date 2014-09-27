cbbdce88b4e01c014e309004bca8c37cda131457
DerManiac <dermaniac@gmail.com>
2014-09-27 11:14:48 +0200

    Added formatting rules

---
+++ src/com/gamelabgraz/jam/tpbjg/Controls.java
@@ -0,0 +1,7 @@
+package com.gamelabgraz.jam.tpbjg;
+
+public class Controls {
+
+  private boolean useGamepad;
+
+}
--- src/com/gamelabgraz/jam/tpbjg/ThePeanutButterJellyGame.java
+++ src/com/gamelabgraz/jam/tpbjg/ThePeanutButterJellyGame.java
@@ -8,33 +8,33 @@ import org.newdawn.slick.SlickException;
 
 public class ThePeanutButterJellyGame extends BasicGame {
 
-	public ThePeanutButterJellyGame() {
-		super("The Peanut Butter Jelly Game");
-	}
+  public ThePeanutButterJellyGame() {
+    super("The Peanut Butter Jelly Game");
+  }
 
-	@Override
-	public void render(GameContainer container, Graphics delta) throws SlickException {
-		
-	}
+  @Override
+  public void render(GameContainer container, Graphics delta) throws SlickException {
 
-	@Override
-	public void init(GameContainer container) throws SlickException {
-		
-	}
- 
-	@Override
-	public void update(GameContainer container, int delta) throws SlickException {
-		System.out.println("Update called with delta: "+delta);
-	}
+  }
 
-	public static void main(String[] args){
-		try {
-			ThePeanutButterJellyGame game = new ThePeanutButterJellyGame();
-			AppGameContainer container = new AppGameContainer(game,800,600,false);
-			container.start();
-		} catch (SlickException e) {
-			e.printStackTrace();
-		}
-	}
-		
+  @Override
+  public void init(GameContainer container) throws SlickException {
+
+  }
+
+  @Override
+  public void update(GameContainer container, int delta) throws SlickException {
+
+  }
+
+  public static void main(String[] args) {
+    try {
+      ThePeanutButterJellyGame game = new ThePeanutButterJellyGame();
+      AppGameContainer container = new AppGameContainer(game, 800, 600, false);
+      container.start();
+    } catch (SlickException e) {
+      e.printStackTrace();
+    }
+  }
+
 }

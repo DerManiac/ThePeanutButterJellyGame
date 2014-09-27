e9220dbf42bd236d849797d93531c1c040ba61a7
Christian <leopold.christian@gmx.at>
2014-09-27 17:39:53 +0300

    Item Types added

---
+++ src/com/gamelabgraz/jam/tpbjg/items/IItemGenerator.java
@@ -0,0 +1,11 @@
+package com.gamelabgraz.jam.tpbjg.items;
+
+/**
+ * @author vinzynth
+ * Sep 27, 2014 - 5:32:50 PM
+ *
+ */
+public interface IItemGenerator {
+
+  
+}
---
+++ src/com/gamelabgraz/jam/tpbjg/items/ItemType.java
@@ -0,0 +1,27 @@
+package com.gamelabgraz.jam.tpbjg.items;
+
+import com.gamelabgraz.jam.tpbjg.ThePeanutButterJellyGame;
+import com.gamelabgraz.jam.tpbjg.items.implementation.CometAction;
+import com.gamelabgraz.jam.tpbjg.items.implementation.EarthAction;
+import com.gamelabgraz.jam.tpbjg.items.implementation.MagnetAction;
+
+/**
+ * @author vinzynth
+ * Sep 27, 2014 - 5:23:52 PM
+ *
+ */
+public enum ItemType {
+  COMET(new CometAction()),
+  EARTH(new EarthAction()),
+  MAGNET(new MagnetAction());
+  
+  private IItemAction action;
+
+  private ItemType(IItemAction action) {
+    this.action = action;
+  }
+  
+  public void process(ThePeanutButterJellyGame game){
+    action.process(game);
+  }
+}

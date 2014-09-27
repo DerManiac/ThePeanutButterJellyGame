e7d5a1a0d5f9a36453f9d5ae5ddadddb5430cd16
Christian <leopold.christian@gmx.at>
2014-09-27 15:18:54 +0300

    Warnings

--- src/com/gamelabgraz/jam/tpbjg/map/xml/implementaion/XMLGameMapDTO.java
+++ src/com/gamelabgraz/jam/tpbjg/map/xml/implementaion/XMLGameMapDTO.java
@@ -2,7 +2,6 @@ package com.gamelabgraz.jam.tpbjg.map.xml.implementaion;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.IntStream;
 
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;

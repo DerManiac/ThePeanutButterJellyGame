c0b7849a75bb035c06a8c69c16e25310d815f0f0
Christian <leopold.christian@gmx.at>
2014-09-27 19:26:35 +0300

    Freeze und Comet Action implemented

--- src/com/gamelabgraz/jam/tpbjg/items/implementation/CometAction.java
+++ src/com/gamelabgraz/jam/tpbjg/items/implementation/CometAction.java
@@ -1,18 +1,33 @@
 package com.gamelabgraz.jam.tpbjg.items.implementation;
 
+import com.gamelabgraz.jam.tpbjg.Player;
 import com.gamelabgraz.jam.tpbjg.ThePeanutButterJellyGame;
 import com.gamelabgraz.jam.tpbjg.items.IItemAction;
 
 public class CometAction implements IItemAction {
 
+  final float speedfactor = 2;
+  
+  float speedDiff;
+  
   /**
    * {@inheritDoc}
    * @see com.gamelabgraz.jam.tpbjg.items.IItemAction#process(com.gamelabgraz.jam.tpbjg.ThePeanutButterJellyGame)
    */
   @Override
-  public void process(ThePeanutButterJellyGame game) {
-    // TODO Auto-generated method stub
-    
+  public void startEffect(ThePeanutButterJellyGame game, Player effector) {
+    float normalspeed = effector.getSpeed();
+    speedDiff = normalspeed * (speedfactor - 1);
+    effector.setSpeed(effector.getSpeed() + speedDiff);
+  }
+
+  /**
+   * {@inheritDoc}
+   * @see com.gamelabgraz.jam.tpbjg.items.IItemAction#endEffect(com.gamelabgraz.jam.tpbjg.ThePeanutButterJellyGame, com.gamelabgraz.jam.tpbjg.Player)
+   */
+  @Override
+  public void endEffect(ThePeanutButterJellyGame game, Player effector) {
+    effector.setSpeed(effector.getSpeed() - speedDiff);
   }
 
 }

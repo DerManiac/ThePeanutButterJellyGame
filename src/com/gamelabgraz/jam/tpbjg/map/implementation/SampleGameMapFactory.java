0165467107e9f2c1330475bcd37aafdca8072f01
Christian <leopold.christian@gmx.at>
2014-09-27 15:21:42 +0300

    Sample Game Map Factory

---
+++ src/com/gamelabgraz/jam/tpbjg/map/implementation/SampleGameMapFactory.java
@@ -0,0 +1,28 @@
+package com.gamelabgraz.jam.tpbjg.map.implementation;
+
+import at.chrl.nutils.Rnd;
+
+import com.gamelabgraz.jam.tpbjg.map.FieldType;
+import com.gamelabgraz.jam.tpbjg.map.IGameMap;
+import com.gamelabgraz.jam.tpbjg.map.IGameMapFactory;
+import com.gamelabgraz.jam.tpbjg.map.NoGameMapFoundException;
+
+/**
+ * @author vinzynth
+ * Sep 27, 2014 - 3:19:50 PM
+ *
+ */
+public class SampleGameMapFactory implements IGameMapFactory {
+
+  /**
+   * {@inheritDoc}
+   * @see com.gamelabgraz.jam.tpbjg.map.IGameMapFactory#getGameMap(int)
+   */
+  @Override
+  public IGameMap getGameMap(int mapId) throws NoGameMapFoundException {
+    IGameMap gm = new GameMap(0, 20, 30);
+    gm.foreachField((x,y,type) -> gm.setField(x, y, FieldType.values()[Rnd.nextInt(FieldType.values().length)]));
+    return gm;
+  }
+
+}

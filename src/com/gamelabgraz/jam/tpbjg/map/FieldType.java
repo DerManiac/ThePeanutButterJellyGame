23deffa3c5a7d9b4f1d5d3f7461a8884cb9f1c57
DerManiac <dermaniac@gmail.com>
2014-09-27 14:17:20 +0200

    Added sprite loading for field types (untested

--- src/com/gamelabgraz/jam/tpbjg/map/FieldType.java
+++ src/com/gamelabgraz/jam/tpbjg/map/FieldType.java
@@ -1,9 +1,30 @@
 package com.gamelabgraz.jam.tpbjg.map;
 
+import org.newdawn.slick.Image;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.SpriteSheet;
+
 /**
  * @author vinzynth Sep 27, 2014 - 12:06:14 PM
  *
  */
 public enum FieldType {
-  EMPTY, WALL;
+  EMPTY("assets/graphics/ground.png", 64, 64, 0, 0), WALL("assets/graphics/walls.png", 64, 64, 0, 0);
+
+  private Image background;
+
+  private FieldType(String spritePath, int width, int height, int x, int y) {
+    SpriteSheet backgroundsprites;
+    try {
+      backgroundsprites = new SpriteSheet(spritePath, width, height);
+      background = backgroundsprites.getSprite(x, y);
+    } catch (SlickException e) {
+      System.err.println("Error loading background sprite.");
+      e.printStackTrace();
+    }
+  }
+
+  public Image getBackground() {
+    return background;
+  }
 }

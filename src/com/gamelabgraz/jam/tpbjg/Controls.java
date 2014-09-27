0c7d7d3fcadb8b99e2740d3bb33d67ce5540b099
DerManiac <dermaniac@gmail.com>
2014-09-27 12:13:57 +0200

    Control mapping and default controls

--- src/com/gamelabgraz/jam/tpbjg/Controls.java
+++ src/com/gamelabgraz/jam/tpbjg/Controls.java
@@ -4,4 +4,94 @@ public class Controls {
 
   private boolean useGamepad;
 
+  private int player;
+  private int upButton;
+  private int downButton;
+  private int leftButton;
+  private int rightButton;
+  private int actionButton;
+  private int controllerStartButton;
+
+  public Controls(int player) {
+    // initializing default controls
+    this(player, true);
+  }
+
+  public Controls(int player, boolean useGamepad) {
+    // initializing default controls
+    if (useGamepad) {
+      useGamepad = true;
+      actionButton = 1;
+    } else {
+      if (player == 1) {
+        actionButton = 57; // space
+      } else {
+        actionButton = 28; // enter
+      }
+    }
+
+    // init backup directional keyboard controls even if gamepad is used
+    if (player == 1) {
+      upButton = 17; // W
+      leftButton = 31; // A
+      downButton = 30; // S
+      rightButton = 32; // D
+    } else {
+      upButton = 200; // arrow up
+      leftButton = 203; // arrow left
+      downButton = 208; // arrow down
+      rightButton = 205; // arrow right
+    }
+
+    controllerStartButton = 8; // xbox start button
+  }
+
+  public boolean isUseGamepad() {
+    return useGamepad;
+  }
+
+  public void setUseGamepad(boolean useGamepad) {
+    this.useGamepad = useGamepad;
+  }
+
+  public int getUpButton() {
+    return upButton;
+  }
+
+  public void setUpButton(int upButton) {
+    this.upButton = upButton;
+  }
+
+  public int getDownButton() {
+    return downButton;
+  }
+
+  public void setDownButton(int downButton) {
+    this.downButton = downButton;
+  }
+
+  public int getLeftButton() {
+    return leftButton;
+  }
+
+  public void setLeftButton(int leftButton) {
+    this.leftButton = leftButton;
+  }
+
+  public int getRightButton() {
+    return rightButton;
+  }
+
+  public void setRightButton(int rightButton) {
+    this.rightButton = rightButton;
+  }
+
+  public int getActionButton() {
+    return actionButton;
+  }
+
+  public void setActionButton(int actionButton) {
+    this.actionButton = actionButton;
+  }
+
 }
